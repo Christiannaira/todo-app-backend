c9e405db0c92502ad1577a41600202a48e2450fd
christian <Christianmnaira@gmail.com>
2025-11-30 22:11:13 +0800

    feat: created an item entity(database table)

---
+++ src/main/java/com/todo/Todo/App/entity/Item.java
@@ -0,0 +1,72 @@
+package com.todo.Todo.App.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "items")
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String taskTitle;
+
+    @Column(nullable = false)
+    private String taskDescription;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    public Item() {}
+
+    public Item(String taskTitle, String taskDescription) {
+        this.taskTitle = taskTitle;
+        this.taskDescription = taskDescription;
+    }
+
+
+    // It lets you run code automatically before an entity is saved to the database for the first time.
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+
+    // encapsulation
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTaskTitle() {
+        return taskTitle;
+    }
+
+    public void setTaskTitle(String taskTitle) {
+        this.taskTitle = taskTitle;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+//    public void setCreatedAt(LocalDateTime createdAt) {
+//        this.createdAt = createdAt;
+//    }
+
+}
