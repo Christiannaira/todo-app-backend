472bb1ee31621e2493f1640c7f4f9d7917b2169c
christian <Christianmnaira@gmail.com>
2025-12-01 16:34:16 +0800

    feat: added a delete service logic

--- src/main/java/com/todo/Todo/App/services/impl/ItemServicesImpl.java
+++ src/main/java/com/todo/Todo/App/services/impl/ItemServicesImpl.java
@@ -3,6 +3,7 @@ package com.todo.Todo.App.services.impl;
 import com.todo.Todo.App.dto.ItemRequestDto;
 import com.todo.Todo.App.dto.ItemResponseDto;
 import com.todo.Todo.App.entity.Item;
+import com.todo.Todo.App.exception.ResourceNotFoundException;
 import com.todo.Todo.App.mapper.ItemMapper;
 import com.todo.Todo.App.repository.ItemRepository;
 import com.todo.Todo.App.services.ItemServices;
@@ -33,7 +34,8 @@ public class ItemServicesImpl implements ItemServices {
 
     @Override
     public void deleteItem(Long id) {
-//        Item item = itemRepository.findById(id);
+        Item item = itemRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User not found with id " + id));
+        itemRepository.delete(item);
     }
 
 
