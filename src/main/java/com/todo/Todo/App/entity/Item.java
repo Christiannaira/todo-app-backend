f205daef91d7727750bab3ae777bf568945fa96e
Christian <Christianmnaira@gmail.com>
2025-12-08 00:02:19 +0800

    feat: added new table columns (priorityLevel, dueDate and dueTime)

--- src/main/java/com/todo/Todo/App/entity/Item.java
+++ src/main/java/com/todo/Todo/App/entity/Item.java
@@ -2,7 +2,9 @@ package com.todo.Todo.App.entity;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Entity
 @Table(name = "items")
@@ -15,17 +17,29 @@ public class Item {
     @Column(nullable = false)
     private String taskTitle;
 
-    @Column(nullable = false)
+    @Column(nullable = true)
     private String taskDescription;
 
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
+    @Column(nullable = true)
+    private String priorityLevel;
+
+    @Column(nullable = true)
+    private LocalDate dueDate;
+
+    @Column(nullable = true)
+    private LocalTime dueTime;
+
     public Item() {}
 
-    public Item(String taskTitle, String taskDescription) {
+    public Item(String taskTitle, String taskDescription, String priorityLevel, LocalDate dueDate, LocalTime dueTime) {
         this.taskTitle = taskTitle;
         this.taskDescription = taskDescription;
+        this.priorityLevel = priorityLevel;
+        this.dueDate = dueDate;
+        this.dueTime = dueTime;
     }
 
 
@@ -69,4 +83,27 @@ public class Item {
         this.createdAt = createdAt;
     }
 
+    public String getPriorityLevel() {
+        return priorityLevel;
+    }
+
+    public void setPriorityLevel(String priorityLevel) {
+        this.priorityLevel = priorityLevel;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public LocalTime getDueTime() {
+        return dueTime;
+    }
+
+    public void setDueTime(LocalTime dueTime) {
+        this.dueTime = dueTime;
+    }
 }
