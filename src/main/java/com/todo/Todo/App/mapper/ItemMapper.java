321648a40d4f9f8cf87c6e8003e0bc2e83793751
Christian <Christianmnaira@gmail.com>
2025-12-08 00:04:48 +0800

    feat: implemented the new added to itemMapper

--- src/main/java/com/todo/Todo/App/mapper/ItemMapper.java
+++ src/main/java/com/todo/Todo/App/mapper/ItemMapper.java
@@ -8,7 +8,7 @@ public class ItemMapper {
 
     public static Item toEntity(ItemRequestDto dto) {
         if (dto == null) return null;
-        return new Item(dto.getTaskTitle(), dto.getTaskDescription());
+        return new Item(dto.getTaskTitle(), dto.getTaskDescription(), dto.getPriorityLevel(), dto.getDueDate(), dto.getDueTime());
     }
 
     public static ItemResponseDto toDto(Item item) {
@@ -20,6 +20,9 @@ public class ItemMapper {
         dto.setTaskTitle(item.getTaskTitle());
         dto.setTaskDescription(item.getTaskDescription());
         dto.setCreatedAt(item.getCreatedAt());
+        dto.setPriorityLevel(item.getPriorityLevel());
+        dto.setDueDate(item.getDueDate());
+        dto.setDueTime(item.getDueTime());
 
         return dto;
     }
@@ -27,6 +30,9 @@ public class ItemMapper {
     public static void updateItem(Item entity, ItemRequestDto dto) {
         if (dto.getTaskTitle() != null) entity.setTaskTitle(dto.getTaskTitle());
         if (dto.getTaskDescription() != null) entity.setTaskDescription(dto.getTaskDescription());
+        if (dto.getPriorityLevel() != null) entity.setPriorityLevel(dto.getPriorityLevel());
+        if (dto.getDueDate() != null) entity.setDueDate(dto.getDueDate());
+        if (dto.getDueTime() != null) entity.setDueTime(dto.getDueTime());
     }
 
 }
