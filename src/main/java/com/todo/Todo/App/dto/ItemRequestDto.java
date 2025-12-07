8c46a4c7150bbc15b46fa848101903a4f1584452
Christian <Christianmnaira@gmail.com>
2025-12-08 00:02:55 +0800

    feat: implemented the three new columns in request and response dto's

--- src/main/java/com/todo/Todo/App/dto/ItemRequestDto.java
+++ src/main/java/com/todo/Todo/App/dto/ItemRequestDto.java
@@ -1,16 +1,28 @@
 package com.todo.Todo.App.dto;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class ItemRequestDto {
 
     private String taskTitle;
 
     private String taskDescription;
 
+    private String priorityLevel;
+
+    private LocalDate dueDate;
+
+    private LocalTime dueTime;
+
     public ItemRequestDto(){}
 
-    public ItemRequestDto(String taskTitle, String taskDescription) {
+    public ItemRequestDto(String taskTitle, String taskDescription, String priorityLevel, LocalDate dueDate, LocalTime dueTime) {
         this.taskTitle = taskTitle;
         this.taskDescription = taskDescription;
+        this.priorityLevel = priorityLevel;
+        this.dueDate = dueDate;
+        this.dueTime = dueTime;
     }
 
     public String getTaskTitle() {
@@ -27,5 +39,29 @@ public class ItemRequestDto {
 
     public void setTaskDescription(String taskDescription) {
         this.taskDescription = taskDescription;
+    }
+
+    public String getPriorityLevel() {
+        return priorityLevel;
+    }
+
+    public void setPriorityLevel(String priorityLevel) {
+        this.priorityLevel = priorityLevel;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public LocalTime getDueTime() {
+        return dueTime;
+    }
+
+    public void setDueTime(LocalTime dueTime) {
+        this.dueTime = dueTime;
     }
 }
--- src/main/java/com/todo/Todo/App/dto/ItemResponseDto.java
+++ src/main/java/com/todo/Todo/App/dto/ItemResponseDto.java
@@ -1,6 +1,8 @@
 package com.todo.Todo.App.dto;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class ItemResponseDto {
 
@@ -8,14 +10,20 @@ public class ItemResponseDto {
     private String taskTitle;
     private String taskDescription;
     private LocalDateTime createdAt;
+    private String priorityLevel;
+    private LocalDate dueDate;
+    private LocalTime dueTime;
 
     public ItemResponseDto() {}
 
-    public ItemResponseDto(Long id,  String taskTitle, String taskDescription, LocalDateTime createdAt) {
+    public ItemResponseDto(Long id,  String taskTitle, String taskDescription, LocalDateTime createdAt, String priorityLevel, LocalDate dueDate, LocalTime dueTime) {
         this.id = id;
         this.taskTitle = taskTitle;
         this.taskDescription = taskDescription;
         this.createdAt = createdAt;
+        this.priorityLevel = priorityLevel;
+        this.dueDate = dueDate;
+        this.dueTime = dueTime;
     }
 
     public Long getId() {
@@ -48,5 +56,29 @@ public class ItemResponseDto {
 
     public void setCreatedAt(LocalDateTime createdAt) {
         this.createdAt = createdAt;
+    }
+
+    public String getPriorityLevel() {
+        return priorityLevel;
+    }
+
+    public void setPriorityLevel(String priorityLevel) {
+        this.priorityLevel = priorityLevel;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public LocalTime getDueTime() {
+        return dueTime;
+    }
+
+    public void setDueTime(LocalTime dueTime) {
+        this.dueTime = dueTime;
     }
 }
